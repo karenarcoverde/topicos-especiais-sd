1bf425910c973e8ab83a7b0be2cc3565bef7a203
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2024-06-20 13:07:11 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bk_tree_modification',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='A simple BK-Tree implementation for finding similar words',
     long_description=open('README.md').read(),
